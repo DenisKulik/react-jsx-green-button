62af7963d11715701ddd11cd5fc1326547401843
Denis Kulik <kulikdenis1996x@gmail.com>
2023-02-28 15:22:58 +0700

    add button styles change on click

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const App = () => {
     const [btnText, setBtnText] = React.useState('Click me');
+    const [classesList, setClassesList] = React.useState('');
 
-    const onBtnClick = () => setBtnText('Hello from React');
+    const onBtnClick = () => {
+        setBtnText('Hello from React');
+        setClassesList('green-btn');
+    };
 
-    return (
-        <div className="app">
-            <button onClick={onBtnClick}>{btnText}</button>
-        </div>
-    );
+    return (<div className="app">
+        <button onClick={onBtnClick} className={classesList}>{btnText}</button>
+    </div>);
 };
 
 const container = document.querySelector('#app');
